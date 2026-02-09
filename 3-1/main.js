31cf0e46750beb6d5707573bf28a44107abb7ddb
yuki-mori-0125 <01252000yuuki@gmail.com>
2026-02-09 18:56:13 +0900

    勝ち負けチェック

--- 3-1/main.js
+++ 3-1/main.js
@@ -73,6 +73,17 @@ function judge(_cells) {
   [_cells[0][0], _cells[1][1], _cells[2][2]], // Gのライン
   [_cells[0][2], _cells[1][1], _cells[2][0]]  // Hのライン
 ];
+// 勝ち負けチェック
+for (let line of lines) {
+  const sum = line[0] + line[1] + line[2];
+
+  if (sum === 3) {
+    return WIN_PLAYER_1;
+  }
+
+  if (sum === -3) {
+    return WIN_PLAYER_2;
+  }
 
 
 }
